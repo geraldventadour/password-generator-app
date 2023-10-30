c08f5f811ec12a0a3e3caa2ae2ff6b1ff3845a87
Gérald VENTADOUR <git.s6hk0@aleeas.com>
2023-10-30 14:02:16 +0100

    add emit event

--- src/components/ThePasswordGeneratorCheckboxes.vue
+++ src/components/ThePasswordGeneratorCheckboxes.vue
@@ -1,22 +1,59 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const passwordOptions = ref([])
+const emit = defineEmits(['passwordOptionsChange'])
+
+function handlePasswordOptionsChange() {
+  emit('passwordOptionsChange', passwordOptions)
+}
+</script>
 
 <template>
   <fieldset class="stack">
     <legend class="sr-only">Password must</legend>
     <div class="cluster">
-      <input type="checkbox" name="" id="uppercase" />
+      <input
+        type="checkbox"
+        name="uppercase"
+        id="uppercase"
+        value="uppercase"
+        v-model="passwordOptions"
+        @change="handlePasswordOptionsChange"
+      />
       <label for="uppercase">Include Uppercase Letters</label>
     </div>
     <div class="cluster">
-      <input type="checkbox" name="" id="lowercase" />
+      <input
+        type="checkbox"
+        name="lowercase"
+        id="lowercase"
+        value="lowercase"
+        v-model="passwordOptions"
+        @change="handlePasswordOptionsChange"
+      />
       <label for="lowercase">Include Lowercase Letters</label>
     </div>
     <div class="cluster">
-      <input type="checkbox" name="" id="numbers" />
+      <input
+        type="checkbox"
+        name="numbers"
+        id="numbers"
+        value="numbers"
+        v-model="passwordOptions"
+        @change="handlePasswordOptionsChange"
+      />
       <label for="numbers">Include Numbers</label>
     </div>
     <div class="cluster">
-      <input type="checkbox" name="" id="symbols" />
+      <input
+        type="checkbox"
+        name="symbols"
+        id="symbols"
+        value="symbols"
+        v-model="passwordOptions"
+        @change="handlePasswordOptionsChange"
+      />
       <label for="symbols">Include Symbols</label>
     </div>
   </fieldset>
@@ -26,11 +63,9 @@
 fieldset {
   border: none;
 }
-
 .stack {
   --margin-block-start: 1.25rem;
 }
-
 .cluster {
   --column-gap: 1.5rem;
 }
